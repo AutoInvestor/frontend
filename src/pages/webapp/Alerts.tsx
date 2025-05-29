b9b2eaf6b44cecd92806f059e30f0653250528e7
alvaromaoc <alvaromaoc@gmail.com>
2025-05-29 21:42:15 +0200

    Presentation fixes 2

--- src/pages/webapp/Alerts.tsx
+++ src/pages/webapp/Alerts.tsx
@@ -24,61 +24,62 @@ function RecentAlerts() {
     const [assets, setAssets] = useState<Asset[]>([]);
 
     useEffect(() => {
-        alertsHttpService.getAlerts()
-            .then(alerts => {
-                setAlerts(alerts);
-                Promise.all(alerts
-                    .map(alert => alert.assetId)
-                    .map(assetsHttpService.getAsset)
-                ).then(setAssets);
-            })
-            .catch(err => console.error('Error fetching alerts:', err));
+        async function fetchData() {
+            const alerts = await alertsHttpService.getAlerts();
+            const distinctAssetIds = [...new Set(alerts.map(item => item.assetId))];
+            const assets = await Promise.all(distinctAssetIds.map(assetsHttpService.getAsset));
+            setAlerts(alerts);
+            setAssets(assets);
+        }
+        fetchData().then();
     }, [])
 
     const getAsset = (assetId: string) => {
-        return assets.find(asset => asset.assetId === assetId);
+        const asset = assets.find(asset => asset.assetId === assetId);
+        if (asset) {
+            return asset;
+        }
+        throw new Error(`Asset with id ${assetId} not found`);
     }
 
     return (
         <div className={"flex gap-y-4 flex-col"}>
             {alerts.map((alert) => {
                 const asset = getAsset(alert.assetId);
-                if (asset) {
-                    return (
-                        <div className={"flex flex-row gap-4 items-center"}>
-                            <div className={"rounded-xl bg-neutral-100 w-fit box-border p-3"}>
-                                {alert.type === "BUY"
-                                    ? <ArrowTrendingUpIcon className={"size-6"}/>
-                                    : <ArrowTrendingDownIcon className={"size-6"}/>}
-                            </div>
-                            <div className={"flex-1"}>
-                                <p>
-                                    <Badge variant="outline">
-                                        <span className={'text-neutral-400'}>{asset.mic}</span>
-                                        <span className="ps-0.5 font-medium">{asset.ticker}</span>
-                                    </Badge>
-                                </p>
-                                <p className={"font-light text-neutral-500 pt-1"}>
-                                    {alert.type === "BUY"
-                                        ? "Technical indicators sugests buying opportunity"
-                                        : "Technical indicators sugests selling opportunity"
-                                    }
-                                </p>
-                            </div>
-                            <div className={"text-neutral-500"}>
-                                <small>{alert.date.toLocaleString('en-US', {
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric',
-                                    hour: 'numeric',
-                                    minute: '2-digit',
-                                    second: '2-digit',
-                                    hour12: true,
-                                })}</small>
-                            </div>
+                return (
+                    <div className={"flex flex-row gap-4 items-center"}>
+                        <div className={"rounded-xl bg-neutral-100 w-fit box-border p-3"}>
+                            {alert.type === "BUY"
+                                ? <ArrowTrendingUpIcon className={"size-6"}/>
+                                : <ArrowTrendingDownIcon className={"size-6"}/>}
                         </div>
-                    )
-                }
+                        <div className={"flex-1"}>
+                            <p>
+                                <Badge variant="outline">
+                                    <span className={'text-neutral-400'}>{asset.mic}</span>
+                                    <span className="ps-0.5 font-medium">{asset.ticker}</span>
+                                </Badge>
+                            </p>
+                            <p className={"font-light text-neutral-500 pt-1"}>
+                                {alert.type === "BUY"
+                                    ? "Technical indicators sugests buying opportunity"
+                                    : "Technical indicators sugests selling opportunity"
+                                }
+                            </p>
+                        </div>
+                        <div className={"text-neutral-500"}>
+                            <small>{alert.date.toLocaleString('en-US', {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric',
+                                hour: 'numeric',
+                                minute: '2-digit',
+                                second: '2-digit',
+                                hour12: true,
+                            })}</small>
+                        </div>
+                    </div>
+                )
             })}
         </div>
     )
